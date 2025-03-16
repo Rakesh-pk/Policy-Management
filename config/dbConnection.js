d5c99b78a0d143371adb7302bd541b35bb09521d
Rakesh-pk <80663593+Rakesh-pk@users.noreply.github.com>
2025-03-16 12:05:04 +0530

    Update dbConnection.js

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -6,7 +6,7 @@ dotenv.config()
 const connectDB = async ()=>{
     
     try {
-        await mongoose.connect(process.env.MONGO_URI ||  'mongodb://localhost:27017/Policy-Mgmt')
+        await mongoose.connect(process.env.MONGO_URI)
         console.log('Connected to Mongodb')
     } catch (error) {
         console.log('Failed to Connect MongoDB database')
